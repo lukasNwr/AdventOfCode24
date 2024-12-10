ad7d4f9951c315119c542b391c0ae380b8185513
Lukáš Novoroľník <lukas.novorolnik@gmail.com>
2024-12-10 13:20:57 +0100

    day 7, part 1

---
+++ 07/bridge-repair.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func findCombination(numbers []int, testValue int) bool {
+	var backtrack func(index, currentValue int, currentOp rune) bool
+	backtrack = func(index, currentValue int, currentOp rune) bool {
+
+		// Check if the value is what am I looking for when I reach the end of the array
+		if index == len(numbers) {
+			return currentValue == testValue
+		}
+
+		operators := []rune{'+', '*'}
+
+		for _, op := range operators {
+			// Initial number, just do the backtrack from next number
+			if index == 0 {
+				if backtrack(index+1, numbers[index], op) {
+					return true
+				}
+			} else {
+				newValue := 0
+				switch currentOp {
+				case '+':
+					newValue = currentValue + numbers[index]
+				case '*':
+					newValue = currentValue * numbers[index]
+				}
+
+				if backtrack(index+1, newValue, op) {
+					return true
+				}
+			}
+		}
+
+		return false
+	}
+
+	return backtrack(0, 0, '+') || backtrack(0, 0, '*')
+}
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	totalCalibration := 0
+
+	for scanner.Scan() {
+		var numbers []int
+
+		line := scanner.Text()
+		processed := strings.Split(line, ":")
+		testValue, _ := strconv.Atoi(processed[0])
+
+		nums := strings.Split(processed[1], " ")
+		for _, v := range nums {
+			num, _ := strconv.Atoi(v)
+			numbers = append(numbers, num)
+		}
+
+		if findCombination(numbers, testValue) {
+			totalCalibration += testValue
+		}
+	}
+	fmt.Println("Total calibration: ", totalCalibration)
+}
